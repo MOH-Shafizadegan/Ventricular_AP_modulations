9eb98bfb6557642d37a6225d75cce27f246c7d59
MOH-Shafizadegan <mh.shafizadegan@gmail.com>
2025-04-01 10:54:43 +1300

    Updating the results by adding effects of two parameters

--- Code/two_param_analysis.py
+++ Code/two_param_analysis.py
@@ -1,11 +1,12 @@
 import numpy as np
 import random
 import os
+import pandas as pd
 from Functions.OpenCor_Py.opencor_helper import SimulationHelper
 import matplotlib
 import matplotlib.pyplot as plt
 from SALib.sample import latin
-import time
+from scipy.stats import qmc
 matplotlib.use('Agg')
 from mpl_toolkits.mplot3d import Axes3D
 import warnings
@@ -31,8 +32,11 @@ def run_and_get_results(param_vals):
 working_dir = os.path.join(os.path.dirname(__file__))
 model_path = os.path.join(working_dir, "Models/ToRORd_dynCl_endo.cellml")
 
-# Setting the output directory
-output_file_path = "outputs/two_param_analysis"
+# Setting the output
+output_type = 'ADP90'
+repolarization_level = int(re.search(r'\d+', output_type).group())
+
+output_file_path = "outputs/two_param_analysis/" + output_type
 if not os.path.exists(output_file_path):
     os.makedirs(output_file_path)
 
@@ -41,6 +45,7 @@ pre_time = 0
 sim_time = 1000
 dt = 1
 n_mem_plot = 5
+sample_type = 'qmc'
 
 # Create simulator object
 sim_object = SimulationHelper(model_path, dt, sim_time, solver_info={'MaximumStep':0.001, 'MaximumNumberOfSteps':500000}, pre_time=pre_time)
@@ -57,7 +62,14 @@ problem = {
     'bounds': list(zip(param_vals_mins, param_vals_maxs))
 }
 num_samples = 200
-samples = latin.sample(problem, num_samples)
+if sample_type == "lhs":
+    samples = latin.sample(problem, num_samples)
+
+elif sample_type == "qmc":
+    sampler = qmc.Sobol(d=problem['num_vars'], scramble=True)  # Scrambled Sobol' for better uniformity
+    qmc_samples = sampler.random(num_samples)
+    samples = qmc.scale(qmc_samples, [b[0] for b in problem['bounds']], [b[1] for b in problem['bounds']])
+
 
 output_names = ['membrane/v']
 
@@ -65,7 +77,7 @@ output_names = ['membrane/v']
 init_param_vals = sim_object.get_init_param_vals(param_names)
 y, t = run_and_get_results(init_param_vals)
 orig_v = np.squeeze(y)
-orig_ADP90 = calculate_apd(t, orig_v, 90, depolarization_threshold=-20)
+orig_ADP90 = calculate_apd(t, orig_v, repolarization_level, depolarization_threshold=-20)
 
 
 param_combinations = list(itertools.combinations(range(len(param_names)), 2))
@@ -80,7 +92,7 @@ for combo in param_combinations:
     sim_object.set_param_vals(param_names, init_param_vals)
 
     mem_Vs = []
-    APD90s = []
+    ADPs = []
 
     for s in range(len(samples)):
         current_param_val = init_param_vals.copy()        
@@ -95,21 +107,21 @@ for combo in param_combinations:
 
         # Store results
         mem_Vs.append(v)
-        APD90s.append(calculate_apd(t, v, 90, depolarization_threshold=-20))
+        ADPs.append(calculate_apd(t, v, repolarization_level, depolarization_threshold=-20))
 
         print(f"Params: {param_subset} - Iteration {s+1}/{len(samples)}.")
 
 
-    # Save DataFrame with parameter values and APD90
+    # Save DataFrame with parameter values and APD
+    param_labels = [re.search(r'(?<=/)(.*)', str(param)).group(1) if '/' in param else param for param in param_subset]
+    param_label = "_".join(param_labels)
+
     df = pd.DataFrame(samples[:, combo], columns=param_labels)
-    df['APD90'] = APD90s
+    df[output_type] = ADPs
     
     if not os.path.exists(f'{output_file_path}/dataset'):
         os.makedirs(f'{output_file_path}/dataset')
     df.to_csv(f"{output_file_path}/dataset/{param_label}_data.csv", index=False)
-
-    param_labels = [re.search(r'(?<=/)(.*)', str(param)).group(1) if '/' in param else param for param in param_subset]
-    param_label = "_".join(param_labels)
 
     # plot the membrane action potentials
     selected_indices = random.sample(range(len(mem_Vs)), min(n_mem_plot, len(mem_Vs)))
@@ -133,7 +145,7 @@ for combo in param_combinations:
     # normalize data
     scaler = MinMaxScaler(feature_range=(0, 1)) 
     X = scaler.fit_transform(samples[:, combo])
-    y = scaler.fit_transform(np.array(APD90s).reshape(-1, 1)).flatten()  
+    y = scaler.fit_transform(np.array(ADPs).reshape(-1, 1)).flatten()  
 
 
     model = LinearRegression()
@@ -149,12 +161,12 @@ for combo in param_combinations:
     ax.scatter(X[:, 0], X[:, 1], y, color='blue', label="Data")
     ax.set_xlabel(f"Normalized {param_subset[0]}")
     ax.set_ylabel(f"Normalized {param_subset[1]}")
-    ax.set_zlabel("Normalized APD90")
+    ax.set_zlabel(f"Normalized {output_type}")
 
     # Annotate with regression equation
-    eq_text = f"APD90 = {coef1:.3f} * {param_labels[0]} + {coef2:.3f} * {param_labels[1]} + {intercept:.3f}"
+    eq_text = f"{output_type} = {coef1:.3f} * {param_labels[0]} + {coef2:.3f} * {param_labels[1]} + {intercept:.3f}"
     ax.text2D(0.05, 0.95, eq_text, transform=ax.transAxes, fontsize=10, bbox=dict(facecolor='white', alpha=0.5))
 
     ax.set_title(f"Linear Regression: {param_labels[0]} and {param_labels[1]}")
-    plt.savefig(output_file_path + "/" + param_label + "_scatter3d.png")
+    plt.savefig(output_file_path + "/" + f"{output_type}_{sample_type}_" + param_label + "_scatter3d.png")
     plt.clf()
